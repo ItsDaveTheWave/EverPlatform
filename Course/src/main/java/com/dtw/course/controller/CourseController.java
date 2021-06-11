cfe5b6690af00e323c1a76e4d019c1bfb50295d4
David de Castro Hernandez <itsdavidch@gmail.com>
2021-06-11 07:18:43 +0200

    Now using dto in controller

--- Course/src/main/java/com/dtw/course/controller/CourseController.java
+++ Course/src/main/java/com/dtw/course/controller/CourseController.java
@@ -3,6 +3,8 @@ package com.dtw.course.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dtw.course.dto.CourseDto;
 import com.dtw.course.entity.Course;
 import com.dtw.course.service.CourseService;
 
@@ -22,28 +25,34 @@ public class CourseController {
 
 	@Autowired
 	private CourseService courseService;
-	
+
+	@Autowired
+	@Qualifier("mvcConversionService")
+	private ConversionService conversionService;
+
 	@GetMapping
-	public ResponseEntity<List<Course>> getAll() {
-		
-		return ResponseEntity.ok(courseService.getAll());
+	public ResponseEntity<List<CourseDto>> getAll() {
+
+		return ResponseEntity.ok(courseService.toDtoList(courseService.getAll()));
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Course> getOne(@PathVariable Long id) {
-		
-		return ResponseEntity.ok(courseService.getOne(id));
+	public ResponseEntity<CourseDto> getOne(@PathVariable Long id) {
+
+		return ResponseEntity.ok(conversionService.convert(courseService.getOne(id), CourseDto.class));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Course> create(@RequestBody Course course) {
-		
-		return new ResponseEntity<Course>(courseService.create(course), HttpStatus.CREATED);
+	public ResponseEntity<CourseDto> create(@RequestBody CourseDto course) {
+
+		return new ResponseEntity<CourseDto>(conversionService
+				.convert(courseService.create(conversionService.convert(course, Course.class)), CourseDto.class),
+				HttpStatus.CREATED);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		
+
 		courseService.delete(id);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
--- Course/src/main/java/com/dtw/course/service/CourseService.java
+++ Course/src/main/java/com/dtw/course/service/CourseService.java
@@ -1,10 +1,14 @@
 package com.dtw.course.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
+import com.dtw.course.dto.CourseDto;
 import com.dtw.course.entity.Course;
 import com.dtw.course.repo.CourseRepo;
 
@@ -13,20 +17,42 @@ public class CourseService {
 
 	@Autowired
 	private CourseRepo courseRepo;
-	
+
+	@Autowired
+	@Qualifier("mvcConversionService")
+	private ConversionService conversionService;
+
 	public List<Course> getAll() {
 		return courseRepo.findAll();
 	}
-	
+
 	public Course getOne(Long id) {
 		return courseRepo.findById(id).get();
 	}
-	
+
 	public Course create(Course course) {
 		return courseRepo.save(course);
 	}
-	
+
 	public void delete(Long id) {
 		courseRepo.deleteById(id);
+	}
+
+	public List<CourseDto> toDtoList(List<Course> courses) {
+
+		List<CourseDto> res = new ArrayList<>();
+		for (Course course : courses) {
+			res.add(conversionService.convert(course, CourseDto.class));
+		}
+		return res;
+	}
+
+	public List<Course> toEntityList(List<CourseDto> courses) {
+
+		List<Course> res = new ArrayList<>();
+		for (CourseDto course : courses) {
+			res.add(conversionService.convert(course, Course.class));
+		}
+		return res;
 	}
 }
