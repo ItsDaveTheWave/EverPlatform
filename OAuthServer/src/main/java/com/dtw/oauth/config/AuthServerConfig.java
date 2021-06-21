13df6faa31c8468997940c5d2ee22b20c5e512ae
David de Castro Hernandez <itsdavidch@gmail.com>
2021-06-21 11:59:10 +0200

    Added authentication manager to OAuthServer

--- OAuthServer/src/main/java/com/dtw/oauth/config/AuthServerConfig.java
+++ OAuthServer/src/main/java/com/dtw/oauth/config/AuthServerConfig.java
@@ -2,10 +2,12 @@ package com.dtw.oauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
@@ -14,6 +16,9 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -36,5 +41,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 			.accessTokenValiditySeconds(5000)
 			.refreshTokenValiditySeconds(50000)
 			.autoApprove(true);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
 	}
 }
--- OAuthServer/src/main/java/com/dtw/oauth/config/WebSecurityConfig.java
+++ OAuthServer/src/main/java/com/dtw/oauth/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.dtw.oauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,5 +37,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder(){ 
         return new BCryptPasswordEncoder(); 
+    }
+    
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
