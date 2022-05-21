714856e95918fd3309641b9ea882b50de7451876
David <itsdavidch@gmail.com>
2022-05-21 06:39:38 +0200

    Added AccessDenied to exceptionHandler

--- Assignment/src/main/java/com/dtw/assignment/error/MyRestExceptionHandler.java
+++ Assignment/src/main/java/com/dtw/assignment/error/MyRestExceptionHandler.java
@@ -2,13 +2,24 @@ package com.dtw.assignment.error;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.dtw.errorHandler.RestExceptionHandler;
+import com.dtw.errorHandler.error.ApiError;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class MyRestExceptionHandler extends RestExceptionHandler {
 
-	
+	// Access denied exception
+	@ExceptionHandler(AccessDeniedException.class)
+	protected ResponseEntity<ApiError> handleAccessDenied(AccessDeniedException ex) {
+		ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED);
+		apiError.setMessage(ex.getMessage());
+		return super.buildResponseEntityApiError(apiError);
+	}
 }
